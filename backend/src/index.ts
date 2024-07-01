00901f1e4e6bcf4b80708f532cd573098e07a47d
Hasika <hasika20.2005@gmail.com>
2024-07-01 11:52:22 +0530

    user registration authentication done

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,29 +2,21 @@ import express, { Request, Response } from "express"
 import cors from "cors"
 import mongoose from "mongoose"
 import dotenv from "dotenv"
+import userRoutes from "./routes/users"
 
-// Explicitly load .env file
 dotenv.config()
 
-const mongoUri = process.env.MONGO_CONNECTION_STRING
-
-if (!mongoUri) {
-	throw new Error("MONGO_CONNECTION_STRING is not defined")
-}
-
 mongoose
-	.connect(mongoUri)
-	.then(() => console.log("MongoDB connected"))
-	.catch((err) => console.error("MongoDB connection error:", err))
+	.connect(process.env.MONGO_CONNECTION_STRING as string)
+	.then(() => console.log("Connected to MongoDB"))
+	.catch((err) => console.error("Failed to connect to MongoDB", err))
 
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
 
-app.get("/api/test", async (req: Request, res: Response) => {
-	res.json({ message: "Hello from express end point!" })
-})
+app.use("/api/users", userRoutes)
 
 app.listen(3000, () => {
 	console.log("Server is running on localhost:3000")
