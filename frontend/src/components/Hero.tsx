2652d4833ef0881f570380e6aa77a8fb859632df
Hasika <hasika20.2005@gmail.com>
2024-07-01 09:07:12 +0530

    frontend header footer hero page done

---
+++ frontend/src/components/Hero.tsx
@@ -0,0 +1,16 @@
+const Hero = () => {
+	return (
+		<div className='bg-cyan-950 pb-16'>
+			<div className='container mx-auto flex flex-col gap-2'>
+				<h1 className='text-5xl text-white font-bold'>
+					Your perfect getaway awaits!
+				</h1>
+				<p className='text-xl text-white'>
+					The world is waiting, at prices you'll love...
+				</p>
+			</div>
+		</div>
+	)
+}
+
+export default Hero
